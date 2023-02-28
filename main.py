fbb2f5ac3cb177403ae7a123f80a07373b57da6f
J'Fang <104315180+CoralAquae@users.noreply.github.com>
2023-02-28 16:41:58 +0800

    ver2

--- main.py
+++ main.py
@@ -8,23 +8,20 @@ from torch.utils.data import Dataset, DataLoader, TensorDataset
 
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # device = torch.device('cpu')
     # read data
     f = open('D:\\学校\\实习\\toutiao_cat_data.txt', 'r', encoding='utf-8')
     text = get_examples(f)
-    # text = text[:1000]
     # 划分训练集测试集
-    x_train, x_test = train_test_split(text[:], test_size=0.3, stratify=text['index'])
+    x_train, x_test = train_test_split(text[:], test_size=0.1, stratify=text['index'])
 
     # import model
-    tokenizer = BertTokenizer.from_pretrained('D:\\学校\\学年论文\\1.16\\chinese_rbtl3_pytorch')
-    model = BertForSequenceClassification.from_pretrained('D:\\学校\\学年论文\\1.16\\chinese_rbtl3_pytorch', num_labels=16).to(device)
-    dropout = nn.Dropout(0.4, inplace=False)
+    tokenizer = BertTokenizer.from_pretrained('D:\\学校\\学年论文\\1.16\\chinese_roberta_wwm_ext_pytorch')
+    model = BertForSequenceClassification.from_pretrained('D:\\学校\\学年论文\\1.16\\chinese_roberta_wwm_ext_pytorch', num_labels=16).to(device)
+    dropout = nn.Dropout(0.05, inplace=False)
     model.dropout = dropout
-    # model = Classifier_model()
     print(model)
     for i, param in enumerate(model.parameters()):
-        if i < 30:  # 参数冻结
+        if i < 15:  # 参数冻结
             param.requires_grad = False
 
     train_encoding = tokenizer(np.array(x_train[['text']].T).tolist()[0], truncation=True, padding=True, max_length=64)
@@ -38,7 +35,7 @@ if __name__ == '__main__':
 
     # 优化方法
     # optim = AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=2e-6)
-    optim = AdamW(model.parameters(), lr=2e-5, eps = 1e-8)
+    optim = AdamW(model.parameters(), lr=3e-5, eps = 1e-8)
     # total_steps = len(train_loader) * 1
     # scheduler = get_linear_schedule_with_warmup(optim,
     #                                            num_warmup_steps=0,
